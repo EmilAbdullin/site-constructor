22193f9df7a2488f1dd3533fc18079b5c64d555f
e.abdullin <emil.abdu11in@yandex.ru>
2020-08-21 12:23:27 +0300

    frontend_part created structure

---
+++ src/model.js
@@ -0,0 +1,16 @@
+export const model = [
+    {type:'title', value:'Test Title',options:{
+        tag:'h2',
+        styles:'background: tomato; color:#fff; text-align:center;'
+    }},
+    {type:'text', value: 'Test text', options:{
+        styles:'background: darkblue; color:yellow;'
+    }},
+    {type:'textColumns', value:[
+        '1 text',
+        '2 text',
+        '3 text'
+    ],options:{
+        styles:'padding:1rem;'
+    }}
+]
